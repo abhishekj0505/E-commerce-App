1ab10507358b1a6796ab13aae4e37661ac2e9039
abhishek jaiswal <abhishek.jaiswal5385@gmail.com>
2024-10-17 10:40:54 +0530

    added active pages link

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,94 +1,152 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import SearchBar from "../searchBar/SearchBar";
 import { useSelector } from "react-redux";
 
-
 const NavBar = () => {
+  const user = JSON.parse(localStorage.getItem("users"));
 
-    const user = JSON.parse(localStorage.getItem('users'))
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const cartItems = useSelector((state) => state.cart);
 
-    const cartItems = useSelector(state=>state.cart)
+  const logOut = () => {
+    localStorage.clear("users");
+    navigate("/login");
+  };
 
-    const logOut = ()=>{
-        localStorage.clear('users')
-        navigate('/login')
-    }
+  // navList Data
+  const navList = (
+    <ul className="flex space-x-7 text-white font-medium text-md px-5 ">
+      {/* Home */}
+      <li>
+        <NavLink
+          className={({ isActive }) =>
+            `${
+              isActive
+                ? "font-bold border-solid border border-sky p-2 rounded-md"
+                : "text-white"
+            }`
+          }
+          to={"/"}
+        >
+          Home
+        </NavLink>
+      </li>
 
-    // navList Data
-    const navList = (
-        <ul className="flex space-x-3 text-white font-medium text-md px-5 ">
-            {/* Home */}
-            <li>
-                <Link to={'/'}>Home</Link>
-            </li>
+      {/* All Product */}
+      <li>
+        <NavLink className={({ isActive }) =>
+            `${
+              isActive
+                ? "font-bold border-solid border border-sky p-2 rounded-md"
+                : "text-white"
+            }`
+          } to={"/allproduct"}>All Product</NavLink >
+      </li>
 
-            {/* All Product */}
-            <li>
-                <Link to={'/allproduct'}>All Product</Link>
-            </li>
+      {/* Signup */}
+      {!user ? (
+        <li>
+          <NavLink className={({ isActive }) =>
+            `${
+              isActive
+                ? "font-bold border-solid border border-sky p-2 rounded-md"
+                : "text-white"
+            }`
+          } to={"/signup"}>Signup</NavLink>
+        </li>
+      ) : (
+        ""
+      )}
 
-            {/* Signup */}
-            {!user ? <li>
-                <Link to={'/signup'}>Signup</Link>
-            </li> : ''}
+      {/* Login*/}
+      {!user ? (
+        <li>
+          <NavLink className={({ isActive }) =>
+            `${
+              isActive
+                ? "font-bold border-solid border border-sky p-2 rounded-md"
+                : "text-white"
+            }`
+          } to={"/login"}>Login</NavLink>
+        </li>
+      ) : (
+        ""
+      )}
 
-            {/* Login*/}
-            {!user ? <li>
-                <Link to={'/login'}>Login</Link>
-            </li> : ''}
+      {/* User */}
+      {user?.role == "user" && (
+        <li>
+          <NavLink className={({ isActive }) =>
+            `${
+              isActive
+                ? "font-bold border-solid border border-sky p-2 rounded-md"
+                : "text-white"
+            }`
+          } to={"/user-dashboard"}>{user.name}</NavLink>
+        </li>
+      )}
 
-            {/* User */}
-            {
-                user?.role == 'user' && <li>
-                <Link to={'/user-dashboard'}>{user.name}</Link>
-            </li>
-            }
+      {/* Admin */}
+      {user?.role == "admin" && (
+        <li>
+          <NavLink className={({ isActive }) =>
+            `${
+              isActive
+                ? "font-bold border-solid border border-sky p-2 rounded-md"
+                : "text-white"
+            }`
+          } to={"/admin-dashboard"}>{user.name}</NavLink>
+        </li>
+      )}
 
-            {/* Admin */}
-            {
-                user?.role == 'admin' && <li>
-                <Link to={'/admin-dashboard'}>{user.name}</Link>
-            </li>
-            }
+      {/* logout */}
+      {user && (
+        <li className="cursor-pointer" onClick={logOut}>
+          Logout
+        </li>
+      )}
 
-            {/* logout */}
-            {
-                user &&  <li className="cursor-pointer" onClick={logOut}>
-                Logout
-            </li>
-            }
+      {/* Cart */}
+      <li>
+        <NavLink className={({ isActive }) =>
+            `${
+              isActive
+                ? "font-bold border-solid border border-sky p-2 rounded-md"
+                : "text-white"
+            }`
+          } to={"/cart"}>Cart({cartItems.length})</NavLink>
+      </li>
+    </ul>
+  );
+  return (
+    <nav className="bg-pink-600 sticky top-0 ">
+      {/* main  */}
+      <div className="lg:flex lg:justify-between items-center py-3 lg:px-3 ">
+        {/* left  */}
+        <div className="left py-3 lg:py-0">
+          <Link to={"/"}>
+            <h2
+              onClick={() => {
+                navigate("/");
+              }}
+              className="font-bold text-white text-2xl text-center"
+            >
+              CARTIFY
+            </h2>
+          </Link>
+        </div>
 
-            {/* Cart */}
-            <li>
-                <Link to={'/cart'}>
-                    Cart({cartItems.length})
-                </Link>
-            </li>
-        </ul>
-    )
-    return (
-        <nav className="bg-pink-600 sticky top-0 ">
-            {/* main  */}
-            <div className="lg:flex lg:justify-between items-center py-3 lg:px-3 ">
-                {/* left  */}
-                <div className="left py-3 lg:py-0">
-                    <Link to={'/'}>
-                    <h2 onClick={()=>{navigate('/')}} className="font-bold text-white text-2xl text-center">CARTIFY</h2>
-                    </Link>
-                </div>
+        {/* right  */}
+        <div className="right flex justify-center mb-4 lg:mb-0 capitalize">
+          {navList}
+        </div>
 
-                {/* right  */}
-                <div className="right flex justify-center mb-4 lg:mb-0 capitalize">
-                    {navList}
-                </div>
-
-                {/* Search Bar  */}
-                <SearchBar />
-            </div>
-        </nav>
-    );
-}
+        {/* Search Bar  */}
+        <SearchBar />
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
